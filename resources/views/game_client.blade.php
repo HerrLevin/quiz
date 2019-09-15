c0de478a41d9133cc7353487adb4a7f0885f35c9
Thomas Englert <thomas@englert.one>
2019-09-15 15:41:49 +0200

    input Field hinzugefügt

--- resources/views/game_client.blade.php
+++ resources/views/game_client.blade.php
@@ -6,10 +6,13 @@ Gamecode: {{ $code }}
     <div class="row">
         <div class="col-12">
             <h1 class="text-center">Frage 1</h1>
-            <button id="antwort1" type="button" class="btn btn-success btn-lg btn-block" disabled>Antwort 1</button>
-            <button id="antwort2" type="button" class="btn btn-danger btn-lg btn-block" disabled>Antwort 2</button>
-            <button id="antwort3" type="button" class="btn btn-warning btn-lg btn-block" disabled>Antwort 3</button>
-            <button id="antwort4" type="button" class="btn btn-info btn-lg btn-block" disabled>Antwort 4</button>
+            <button id="antwort1" type="button" class="btn btn-success btn-lg display-none" disabled>Antwort 1</button>
+            <button id="antwort2" type="button" class="btn btn-danger btn-lg display-none" disabled>Antwort 2</button>
+            <button id="antwort3" type="button" class="btn btn-warning btn-lg display-none" disabled>Antwort 3</button>
+            <button id="antwort4" type="button" class="btn btn-info btn-lg display-none" disabled>Antwort 4</button>
+            <form>
+                <input id="zahl" type="number" class="form-control display-none mt-5" disabled></input>
+            </form>
         </div>
         <div class="col-12 mt-5">
             <div id="timer" class="text-center">
